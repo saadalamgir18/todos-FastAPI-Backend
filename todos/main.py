e5970e8c2bcd1d007030e6ca0348bdb9634760f9
saadalamgir18 <saadalamgir18@gmail.com>
2024-05-19 18:35:21 +0500

    created database connection, todo schema using SQLModel, implemented somecrud operations in database

--- todos/main.py
+++ todos/main.py
@@ -1,59 +1,61 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
 import uvicorn
-
+from sqlmodel import Field, SQLModel, create_engine, select, Session
+from dotenv import load_dotenv
+import os
 app = FastAPI()
+load_dotenv()
 
+class TODOTABLE(SQLModel, table=True):
+    id: int = Field(default = None, primary_key = True)
+    task: str
+
+connection_str = os.getenv("DB_URL")
+connection = create_engine(str(connection_str))
+SQLModel.metadata.create_all(connection)
 class Todo(BaseModel):
     id: int
     task: str
 print("app is running")
-todosData = [
-    {
-        "id": 1,
-        "task": "go to gym"
-    },
-    {
-        "id": 2,
-        "task": "go to gym"
-    },
-    {
-        "id": 3,
-        "task": "go to gym"
-    }
-]
+
 @app.get("/getalltodos")
 def gettodo():
-    global todosData
-    return todosData
+    with Session(connection) as session:
+        statment = select(TODOTABLE)
+        results = session.exec(statment)
+        data = results.all()
+        return data if data else {}
 
 @app.post("/addtodo")
 def addtodo(todo:Todo):
-    global todosData
-    todosData.append(todo)
-    return todosData
+    with Session(connection) as session:
+        statment = session.add(TODOTABLE(id = todo.id, task = todo.task))
+        session.commit()
+    return {
+        "message": "task added succefully",
+        "todo": todo
+    }
 
 @app.get("/gettodobyid/{id}")
 def gettodobyid(id:int):
-    global todosData
-    print(todosData)
-    for td in todosData:
-        print(td)
-        if td["id"] == id:
+    with Session(connection) as session:
+        statment = select(TODOTABLE).where(TODOTABLE.id == id)
+        results = session.exec(statment)
+        data = results.all()
+        return data if data else {}
 
-            return td
-        
 @app.put("/updatetodobyid")
 def updatetodobyid(todo:Todo):
-    global todosData
-    print("in update")
-    for td in todosData:
-        if td["id"] == todo.id:
-            td["task"] = todo.task
-            return {"message": "todo updated successfully",
-                    "task":td["task"]} 
-
-
+    with Session(connection) as session:
+        statment = select(TODOTABLE).where(TODOTABLE.id == todo.id)
+        results = session.exec(statment)
+        set_todo = results.one()
+        set_todo.task = todo.task
+        session.add(set_todo)
+        session.commit()
+        session.refresh(set_todo)
+    return set_todo
 
 
 def start():
